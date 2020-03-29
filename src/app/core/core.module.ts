b3823d3a8c20cd714dcbf8d3e033b08ec8e304ab
Carlos K <carloskotachoamd@hotmail.com>
2020-03-29 19:01:58 -0300

    Adicionado primeng, primeicons, módulo/componente navbar

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ],
+  providers: [
+
+  ]
+})
+export class CoreModule { }
