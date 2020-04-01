ec5447bbe29b2565fcf9325536ab0d26b11cda47
Carlos K <carloskotachoamd@hotmail.com>
2020-03-31 22:28:03 -0300

    Atualizando design da lista de mensagens

--- src/app/message/message-search/message-search.component.ts
+++ src/app/message/message-search/message-search.component.ts
@@ -16,14 +16,14 @@ export class MessageSearchComponent implements OnInit {
   ngOnInit() {
 
     this.messages = [
-      { name: 'Katherine May', email: 'katherine@hotmail.com', category: 'Sugestão' },
-      { name: 'Brandon Williams', email: 'brandonw@hotmail.com', category: 'Reclamação' },
-      { name: 'Lara Cohen', email: 'larac@hotmail.com', category: 'Dúvida' },
-      { name: 'Carmelita Marsham', email: 'carmelitamar@hotmail.com', category: 'Elogios' },
-      { name: 'Gvozden Boskovsky', email: 'gvozden@hotmail.com', category: 'Solicitação' },
-      { name: 'Athar Makakooti', email: 'atharm@hotmail.com', category: 'Denúncia' },
-      { name: 'Olisha Farmer', email: 'olishaf@hotmail.com', category: 'Reclamação' },
-      { name: 'Tokunaga Yae', email: 'tokunaga@hotmail.com', category: 'Outro' },
+      { name: 'Katherine May', email: 'katherine@hotmail.com', category: 'Sugestão', status: 'open' },
+      { name: 'Brandon Williams', email: 'brandonw@hotmail.com', category: 'Reclamação', status: 'pending' },
+      { name: 'Lara Cohen', email: 'larac@hotmail.com', category: 'Dúvida', status: 'closed' },
+      { name: 'Carmelita Marsham', email: 'carmelitamar@hotmail.com', category: 'Elogios', status: 'pending' },
+      { name: 'Gvozden Boskovsky', email: 'gvozden@hotmail.com', category: 'Solicitação', status: 'open' },
+      { name: 'Athar Makakooti', email: 'atharm@hotmail.com', category: 'Denúncia', status: 'closed' },
+      { name: 'Olisha Farmer', email: 'olishaf@hotmail.com', category: 'Reclamação', status: 'open' },
+      { name: 'Tokunaga Yae', email: 'tokunaga@hotmail.com', category: 'Outro', status: 'pending' },
     ];
 
   }
