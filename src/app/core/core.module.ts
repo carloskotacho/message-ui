e1104041e201e6da18a070e75dcf3fc875347901
Carlos K <carloskotachoamd@hotmail.com>
2020-04-02 23:32:22 -0300

    Adicionado sidebar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    SidebarModule,
+    ButtonModule
   ],
   declarations: [
     NavbarComponent,
